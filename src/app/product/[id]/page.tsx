f87bb7c377273c96fa39054d767eb2d2d0b50853
amirhoosian <amirhoosian78@gmail.com~>
2025-12-09 17:43:08 +0330

    chore: deploy staging/testing version

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -39,46 +39,50 @@ export default function ProductDetailPage() {
   if (!product) {
     return <div>Product not found</div>;
   }
-  console.log(product);
+
   return (
-    <div className="mx-auto w-full mt-8  text-right">
-      <div className="flex justify-between">
-        <div className="flex flex-1 space-x-10">
-          <div>
-            <div className="w-[500px] bg-white p-5 rounded-2xl border-2 border-stone-300 flex flex-col flex-1 gap-4">
-              <div className="bg-pink-500 text-white px-3 py-1 rounded-lg w-max text-sm">
+    <div className="mx-auto mt-8 w-full text-right">
+      <div className="flex flex-col-reverse justify-between md:flex-row">
+        <div className="flex flex-1 flex-col space-x-10 md:flex-row">
+          <div className="mx-auto w-full md:mx-0 md:w-auto">
+            <div className="mx-auto flex w-full flex-1 flex-col gap-4 rounded-2xl border-2 border-stone-300 bg-white p-5 md:mx-0 md:w-[500px]">
+              <div className="w-max rounded-lg bg-pink-500 px-3 py-1 text-sm text-white">
                 ٪۵۰
               </div>
 
               <div className="space-y-1 text-sm">
                 <p className="text-gray-500">
                   قیمت قبلی:
-                  <span className="line-through text-gray-400">۱۰,۰۰۰,۰۰۰</span>
+                  <span className="text-gray-400 line-through">۱۰,۰۰۰,۰۰۰</span>
                 </p>
 
-                <p className="text-gray-800 font-bold text-lg">
+                <p className="text-lg font-bold text-gray-800">
                   قیمت با تخفیف:
                   <span>۵,۰۰۰,۰۰۰</span>
                 </p>
               </div>
 
-              <p className="text-gray-600 text-sm">موجودی: ۴۰ در انبار</p>
+              <p className="text-sm text-gray-600">موجودی: ۴۰ در انبار</p>
 
-              <button className="w-full bg-green-600 hover:bg-green-700 text-white py-3 rounded-xl text-base">
+              <button className="w-full rounded-xl bg-green-600 py-3 text-base text-white hover:bg-green-700">
                 افزودن به سبد خرید +
               </button>
             </div>
           </div>
-          <div className="flex flex-col gap-10">
-            <p>{product.title}</p>
-            <div className="flex gap-11">
-              <div className="flex flex-col flex-1">
-                <h3>ویژگی ها</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <div className="flex w-full flex-col gap-10 px-2.5 md:w-auto md:px-0">
+            <p className="text-base font-semibold md:text-lg">
+              {product.title}
+            </p>
+            <div className="flex flex-col gap-11 md:flex-row">
+              <div className="flex flex-1 flex-col">
+                <h3 className="text-gray-400">ویژگی ها</h3>
+                <p className="text-gray-400">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
               </div>
-              <div className="flex flex-col flex-1 items-center">
-                <h3>توضیحات</h3>
-                <p>
+              <div className="flex flex-1 flex-col items-end">
+                <h3 className="text-gray-400">توضیحات</h3>
+                <p className="text-gray-400">
                   این محصول ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
                   استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
                   مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی
@@ -99,14 +103,14 @@ export default function ProductDetailPage() {
           </div>
         </div>
 
-        <div className="w-[300px]">
+        <div className="mx-auto w-52 md:mx-0 md:w-[300px]">
           {product.image && (
             <Image
-              src={product.image}
-              alt={product.title}
-              width={300}
               height={300}
-              className="w-full h-auto object-cover"
+              width={300}
+              alt={product.title}
+              className="h-auto w-full object-cover"
+              src={product.image}
               priority
             />
           )}
